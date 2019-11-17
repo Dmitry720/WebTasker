1e81c26e7734612d13a2fbc2a24d0cf267b39ff0
Dmitry Sukhodolsky <capibara1936@gmail.com>
2019-11-17 22:05:31 +0300

    Crerate project and write urls

---
+++ Code/WebTasker/tasker/urls.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # BASE
+    path('', main_url, name='main_url'),
+
+    # USERS
+    path('create-user/', create_user_url, name='create_user_url'),
+    path('profile/', profile_url, name='profile_url'),
+    path('profile/<slug>/', profile_user_url, name='profile_user_url'),
+    path('profile-settings/', profile_settings_url, name='profile_settings_url'),
+
+    # PROJECTS
+    path('project-list/', projects_url, name='projects_url'),
+    path('project/<slug>/', project_details_url, name='project_details_url'),
+
+    # TASK
+    path('task/<slug>/', task_details_url, name='task_details_url'),
+    path('create-task/', create_task_url, name='create_task_url'),
+    path('edit-task/<slug>/', edit_task_url, name='edit_task_url'),
+    path('mark-completed-task/<slug>/', mark_task_complete, name='mark_task_complete'),
+    path('mark-canceled-task/<slug>/', mark_task_canceled, name='mark_task_canceled'),
+
+    # AUTH
+    path('login/', user_login_url, name='user_login_url', kwargs={'redirect_authenticated_user': True}),
+    path('logout/', user_logout_url, name='user_logout_url'),
+
+    # DELETING
+    path('delete-task/<slug>/', delete_task, name='delete_task'),
+]
