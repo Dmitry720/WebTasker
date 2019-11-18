9ae76a83a27ec08111f12c39aa783a586c7188f3
Dmitry Sukhodolsky <capibara1936@gmail.com>
2019-11-18 10:44:31 +0300

    Added encryptors

---
+++ Code/WebTasker/tasker/encryptors.py
@@ -0,0 +1,14 @@
+def user_slug_generate(title):
+    return title.lower().replace(' ', '-')
+
+
+def project_slug_generate(title):
+    return title.lower().replace(' ', '-')
+
+
+def task_slug_generate(title):
+    return title.lower().replace(' ', '-')
+
+
+def subtask_slug_generate(title):
+    return title.lower().replace(' ', '-')
